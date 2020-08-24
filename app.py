60909bcaea6870d44c3a16afe236d08b93b03f36
ice <ice@unixso.com>
2020-08-24 20:45:26 +0800

    add updata

--- app.py
+++ app.py
@@ -48,6 +48,25 @@ def index():
     #return render_template('index.html', user=user, movies=movies)
     return render_template('index.html', movies=movies)
 
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input')
+            return redirect(url_for('edit', movie_id = movie_id))
+
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash('Item updated.')
+        return redirect(url_for('index'))
+    return render_template('edit.html', movie=movie)
+
 @app.route('/home')
 @app.route('/abc')
 def hello():
